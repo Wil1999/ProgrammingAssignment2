480f0423cdd05544cd873d399b2d31bfeb6e8c85
Wil1999 <wil1999_@hotmail.com>
2020-07-02 19:05:13 -0500

    Calculating the inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    x_inv <- NULL
+    set <- function(y){
+        x_inv <<- y
+        x_inv <<- NULL
+    }
+    get <- function() x
+    setInv <- function(inver) x_inv <<- inver
+    getInv <- function() x_inv
+    list(set = set,get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
@@ -12,4 +22,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv()
+    if(!is.null(inv)){
+        message("Obteniendo datos del caché")
+        return(inv)
+    }
+    dato <- x$get()
+    if(det(dato) == 0){
+        message("La matriz no es cuadrada o no es invertible")
+        return(dato)
+    }else{
+        inv <- solve(dato)
+    }
+    x$setInv(inv)
+    inv
 }
